247e67804f7baafed69647d589b7e53b2af822bd
kuchura_d <kuchura.d.wezom@gmail.com>
2016-10-04 10:36:38 +0300

    fixes v1

--- Wezom/Views/MailTemplates/Form.php
+++ Wezom/Views/MailTemplates/Form.php
@@ -19,7 +19,7 @@
                     <div class="form-group">
                         <?php echo \Forms\Builder::bool($obj->status); ?>
                     </div>
-					<div class="form-group">
+                    <div class="form-group">
                         <?php echo \Forms\Builder::input(array(
                             'name' => 'FORM[subject]',
                             'value' => $obj->subject,
@@ -45,137 +45,7 @@
                 </div>
             </div>
             <div class="pageInfo alert alert-info">
-                <div class="rowSection">
-                    <div class="col-md-6"><strong>Доменное имя сайта</strong></div>
-                    <div class="col-md-6">{{site}}</div>
-                </div>
-                <?php if( $obj->id != 15 && $obj->id != 20 && $obj->id != 21 && $obj->id != 26 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>IP</strong></div>
-                        <div class="col-md-6">{{ip}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Текущая дата в формате dd.mm.YYYY</strong></div>
-                        <div class="col-md-6">{{date}}</div>
-                    </div>
-                <?php endif; ?>
-                <?php if ( $obj->id == 1 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Имя</strong></div>
-                        <div class="col-md-6">{{name}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>E-Mail</strong></div>
-                        <div class="col-md-6">{{email}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Текст сообщения</strong></div>
-                        <div class="col-md-6">{{text}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 2 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка для отмены рассылки</strong></div>
-                        <div class="col-md-6">{{link}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>E-Mail</strong></div>
-                        <div class="col-md-6">{{email}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 3 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Имя</strong></div>
-                        <div class="col-md-6">{{name}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Номер телефона</strong></div>
-                        <div class="col-md-6">{{phone}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 4 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка для подтверждения</strong></div>
-                        <div class="col-md-6">{{link}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 5 OR $obj->id == 6 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Новый пароль для входа</strong></div>
-                        <div class="col-md-6">{{password}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 7 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Имя</strong></div>
-                        <div class="col-md-6">{{name}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Город</strong></div>
-                        <div class="col-md-6">{{city}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Текст коментария</strong></div>
-                        <div class="col-md-6">{{text}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Наименование товара</strong></div>
-                        <div class="col-md-6">{{item_name}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка на товар на сайте</strong></div>
-                        <div class="col-md-6">{{link}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка на товар в админ-панели</strong></div>
-                        <div class="col-md-6">{{link_admin}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 8 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Указанный номер телефона</strong></div>
-                        <div class="col-md-6">{{phone}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Наименование товара</strong></div>
-                        <div class="col-md-6">{{item_name}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка на товар на сайте</strong></div>
-                        <div class="col-md-6">{{link}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка на товар в админ-панели</strong></div>
-                        <div class="col-md-6">{{link_admin}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 9 OR $obj->id == 10 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Имя</strong></div>
-                        <div class="col-md-6">{{name}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>E-Mail</strong></div>
-                        <div class="col-md-6">{{email}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Вопрос</strong></div>
-                        <div class="col-md-6">{{question}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Наименование товара</strong></div>
-                        <div class="col-md-6">{{item_name}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка на товар на сайте</strong></div>
-                        <div class="col-md-6">{{link}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка на товар в админ-панели</strong></div>
-                        <div class="col-md-6">{{link_admin}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 11 OR $obj->id == 12 ): ?>
+                <?php if ($obj->id == 1): ?>
                     <div class="rowSection">
                         <div class="col-md-6"><strong>Имя</strong></div>
                         <div class="col-md-6">{{name}}</div>
@@ -185,92 +55,72 @@
                         <div class="col-md-6">{{phone}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Способ оплаты</strong></div>
-                        <div class="col-md-6">{{payment}}</div>
+                        <div class="col-md-6"><strong>Цоколь</strong></div>
+                        <div class="col-md-6">{{cap}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Способ доставки (с номером склада, если нужно)</strong></div>
-                        <div class="col-md-6">{{delivery}}</div>
+                        <div class="col-md-6"><strong>Кол-во</strong></div>
+                        <div class="col-md-6">{{count}}</div>
                     </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Список заказаных товаров</strong></div>
-                        <div class="col-md-6">{{items}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка на заказ в кабинете пользователя</strong></div>
-                        <div class="col-md-6">{{link_user}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Ссылка на заказ в админ-панели</strong></div>
-                        <div class="col-md-6">{{link_admin}}</div>
-                    </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 15 ): ?>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Номер заказа</strong></div>
-                        <div class="col-md-6">{{id}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Фамилия</strong></div>
-                        <div class="col-md-6">{{last_name}}</div>
-                    </div>
+                <?php endif; ?>
+                <?php if ($obj->id == 2): ?>
                     <div class="rowSection">
                         <div class="col-md-6"><strong>Имя</strong></div>
                         <div class="col-md-6">{{name}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Отчество</strong></div>
-                        <div class="col-md-6">{{middle_name}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Список заказаных товаров</strong></div>
-                        <div class="col-md-6">{{items}}</div>
-                    </div>
-                    <div class="rowSection">
-                        <div class="col-md-6"><strong>Стоимость товаров</strong></div>
-                        <div class="col-md-6">{{amount}}</div>
+                        <div class="col-md-6"><strong>Телефон</strong></div>
+                        <div class="col-md-6">{{phone}}</div>
                     </div>
                     <div class="rowSection">
                         <div class="col-md-6"><strong>Скидка</strong></div>
-                        <div class="col-md-6">{{discount}}</div>
+                        <div class="col-md-6">{{deliver}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Стоимость заказа с учетом скидки</strong></div>
-                        <div class="col-md-6">{{real_amount}}</div>
+                        <div class="col-md-6"><strong>% Скидки</strong></div>
+                        <div class="col-md-6">{{percent}}</div>
                     </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 20 || $obj->id == 21 ): ?>
+                <?php endif; ?>
+                <?php if ($obj->id == 2): ?>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Имя, указанное при заказе</strong></div>
+                        <div class="col-md-6"><strong>Имя</strong></div>
                         <div class="col-md-6">{{name}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Фамилия, указанное при заказе</strong></div>
-                        <div class="col-md-6">{{last_name}}</div>
+                        <div class="col-md-6"><strong>Телефон</strong></div>
+                        <div class="col-md-6">{{phone}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Отчество, указанное при заказе</strong></div>
-                        <div class="col-md-6">{{middle_name}}</div>
+                        <div class="col-md-6"><strong>Скидка</strong></div>
+                        <div class="col-md-6">{{deliver}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Номер заказа</strong></div>
-                        <div class="col-md-6">{{id}}</div>
+                        <div class="col-md-6"><strong>% Скидки</strong></div>
+                        <div class="col-md-6">{{percent}}</div>
+                    </div>
+                <?php endif; ?>
+                <?php if ($obj->id == 3 OR $obj->id == 4): ?>
+                    <div class="rowSection">
+                        <div class="col-md-6"><strong>Имя покупателя</strong></div>
+                        <div class="col-md-6">{{name}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Сумма заказа</strong></div>
-                        <div class="col-md-6">{{amount}}</div>
+                        <div class="col-md-6"><strong>Марка автомобиля</strong></div>
+                        <div class="col-md-6">{{mark}}</div>
                     </div>
-                <?php endif ?>
-                <?php if ( $obj->id == 26 ): ?>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>E-Mail</strong></div>
+                        <div class="col-md-6"><strong>Модель автомобиля</strong></div>
+                        <div class="col-md-6">{{model}}</div>
+                    </div>
+                    <div class="rowSection">
+                        <div class="col-md-6"><strong>Email</strong></div>
                         <div class="col-md-6">{{email}}</div>
                     </div>
                     <div class="rowSection">
-                        <div class="col-md-6"><strong>Пароль</strong></div>
-                        <div class="col-md-6">{{password}}</div>
+                        <div class="col-md-6"><strong>Год выпуска</strong></div>
+                        <div class="col-md-6">{{year}}</div>
                     </div>
-                <?php endif ?>
+                <?php endif; ?>
             </div>
         </div>
     </div>
